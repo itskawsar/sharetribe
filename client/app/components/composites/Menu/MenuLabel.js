75f6d7ff317b6220ab7b77a7e5a009990be9f288
Vesa Luusua <vesa.luusua@gmail.com>
2016-06-16 23:41:47 +0300

    remove focus stuff, we are using hover.

--- client/app/components/composites/Menu/MenuLabel.js
+++ client/app/components/composites/Menu/MenuLabel.js
@@ -5,43 +5,10 @@ import hamburgerIcon from './images/hamburgerIcon.svg';
 
 class MenuLabel extends Component {
 
-  constructor(props, context) {
-    super(props, context);
-
-    this.toggleActive = this.toggleActive.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-    this.handleKeyUp = this.handleKeyUp.bind(this);
-    this.handleKeyDown = this.handleKeyDown.bind(this);
-  }
-
-  toggleActive() {
-    this.props.onToggleActive();
-  }
-
-  handleKeyUp(e) {
-    if (e.key === ' ') {
-      this.toggleActive();
-    }
-  }
-
-  handleKeyDown(e) {
-    if (e.key === 'Enter') {
-      this.toggleActive();
-    }
-  }
-
-  handleClick() {
-    this.toggleActive();
-  }
-
   render() {
     return (
       div({
         className: `menu__label ${css.menuLabel} ${this.props.extraClasses}`,
-        onClick: this.handleClick,
-        onKeyUp: this.handleKeyUp,
-        onKeyDown: this.handleKeyDown,
-        onBlur: this.handleBlur,
         tabIndex: '-1',
       }, [
         span({
@@ -56,14 +23,9 @@ class MenuLabel extends Component {
   }
 }
 
-const { bool, func, string } = PropTypes;
-
 MenuLabel.propTypes = {
-  onToggleActive: func.isRequired,
-  isOpen: bool,
-  hasFocus: bool.isRequired,
-  name: string.isRequired,
-  extraClasses: string,
+  name: PropTypes.string.isRequired,
+  extraClasses: PropTypes.string,
 };
 
 export default MenuLabel;
